0e49245154a29003ccd1aa85c994b7984599b225
Boncroix <boncroix@gmail.com>
2024-04-29 21:56:48 +0200

    Initial Commit

---
+++ Marvel_KC/Marvel_KCApp.swift
@@ -0,0 +1,17 @@
+//
+//  Marvel_KCApp.swift
+//  Marvel_KC
+//
+//  Created by Jose Bueno Cruz on 29/4/24.
+//
+
+import SwiftUI
+
+@main
+struct Marvel_KCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            //ContentView()
+        }
+    }
+}
