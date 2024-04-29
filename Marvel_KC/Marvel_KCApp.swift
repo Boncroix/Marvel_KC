ba8c21ae075faca6702bab6314bc06f216bb3a88
Boncroix <boncroix@gmail.com>
2024-04-29 22:34:07 +0200

    Crear estructura de carpetas MVVM

--- Marvel_KC/Marvel_KCApp.swift
+++ Marvel_KC/Marvel_KCApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct Marvel_KCApp: App {
     var body: some Scene {
         WindowGroup {
-            //ContentView()
+            HomeView()
         }
     }
 }
---
+++ Marvel_KC/Presentations/Views/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  Marvel_KC
+//
+//  Created by Jose Bueno Cruz on 29/4/24.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}
